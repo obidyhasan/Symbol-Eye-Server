ddf718f30a8d03d72bfc4e44365c78c727a5719c
obidyhasan <obidyhasan@gmail.com>
2025-01-29 14:36:20 +0600

    include JWT Token, Token verify Middleware, Admin Verify Middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -21,6 +22,51 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // Create Collections
+    const productsCollection = client.db("symbolEyeDB").collection("products");
+    const servicesCollection = client.db("symbolEyeDB").collection("services");
+    const galleryCollection = client.db("symbolEyeDB").collection("gallery");
+    const faqCollection = client.db("symbolEyeDB").collection("faq");
+    const categoriesCollection = client
+      .db("symbolEyeDB")
+      .collection("categories");
+
+    // Verify Token Middleware
+    const verifyToken = (req, res, next) => {
+      if (!req?.headers?.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+
+      const token = req?.headers?.authorization.split(" ")[1];
+      jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decode) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decode = decode;
+        next();
+      });
+    };
+
+    // Admin Verify Middleware
+    const verifyAdmin = async (req, res, next) => {
+      const email = req?.decode?.email;
+      if (email !== process.env.ADMIN_EMAIL) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    // JWT Token Create Api
+    app.post("/api/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET_KEY, {
+        expiresIn: "30 days",
+      });
+      res.send({ token });
+    });
+
+    // --------- REST APIS ----------
+
     await client.connect();
     await client.db("admin").command({ ping: 1 });
     console.log(
